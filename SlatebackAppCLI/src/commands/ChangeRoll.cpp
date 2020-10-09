437a96112d34683e1570f6c2074ab6fc2055088d
filmbrood <filmbrood@gmail.com>
2020-10-08 22:58:05 -0500

    Added 'changeroll' command

--- SlatebackAppCLI/src/commands/ChangeRoll.cpp
+++ SlatebackAppCLI/src/commands/ChangeRoll.cpp
@@ -8,5 +8,25 @@ void ChangeRoll::OnInit()
 
 void ChangeRoll::OnUpdate()
 {
-	std::cout << "Command not yet implemented" << std::endl;
+	LoadSltProjXML("No projects created");
+
+	Camera& activeCamera = Controller::Get().GetActiveCamera();
+
+	std::cout << "Change active roll to: " << std::endl;
+	for (unsigned int i = 0; i < activeCamera.GetRollCount(); i++)
+		std::cout << i << " - " << activeCamera.GetRoll(i).GetID() << std::endl;
+
+	std::string userinput;
+	UserPrompt(userinput, "");
+
+	for (unsigned int i = 0; i < activeCamera.GetRollCount(); i++)
+	{
+		if (userinput == std::to_string(i))
+		{
+			Controller::Get().ChangeActiveRoll(i);
+			std::cout << "Active roll changed to " << Controller::Get().GetActiveRoll().GetID() << std::endl;
+			Serializer::Get().SerializeProjectVector(Controller::Get().GetProjectVector());
+			break;
+		}
+	}
 }
