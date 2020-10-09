00100852920d205578e00bebf217fa8ea2517e52
filmbrood <filmbrood@gmail.com>
2020-10-08 23:15:05 -0500

    Added 'editcamera' and 'editproject' commands

--- SlatebackAppCLI/src/commands/EditCamera.cpp
+++ SlatebackAppCLI/src/commands/EditCamera.cpp
@@ -3,10 +3,49 @@
 void EditCamera::OnInit()
 {
 	SetInput("editcamera");
-	SetDesc("Edit the active camera details. (NOT YET IMPLEMENTED)");
+	SetDesc("Edit the active camera details.");
 }
 
 void EditCamera::OnUpdate()
 {
-	std::cout << "Command not yet implemented" << std::endl;
+	LoadSltProjXML("No projects created");
+
+	Camera& activeCamera = Controller::Get().GetActiveCamera();
+
+	std::cout << "Editing active camera - Input new details now" << std::endl;
+
+	std::string userinput;
+	std::cout << "Current Model: " << activeCamera.GetModel() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeCamera.SetModel(userinput);
+
+	std::cout << "Current ID: " << activeCamera.GetID() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeCamera.SetID(userinput);
+
+	std::cout << "Current Sensor Size: " << activeCamera.GetFilmBack() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeCamera.SetFilmBack(userinput);
+
+	std::cout << "Current Codec: " << activeCamera.GetCodec() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeCamera.SetCodec(userinput);
+
+	std::cout << "Current Lens Set: " << activeCamera.GetLensSet() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeCamera.SetLensSet(userinput);
+
+	std::cout << "Current Operator: " << activeCamera.GetCameraOperator() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeCamera.SetCameraOperator(userinput);
+
+	std::cout << "Current 1st AC: " << activeCamera.GetFirstAssistantCamera() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeCamera.SetFirstAssistantCamera(userinput);
+
+	std::cout << "Current 2nd AC: " << activeCamera.GetSecondAssistantCamera() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeCamera.SetSecondAssistantCamera(userinput);
+
+	Serializer::Get().SerializeProjectVector(Controller::Get().GetProjectVector());
 }
--- SlatebackAppCLI/src/commands/EditProject.cpp
+++ SlatebackAppCLI/src/commands/EditProject.cpp
@@ -2,11 +2,34 @@
 
 void EditProject::OnInit()
 {
-	SetInput("changeroll");
-	SetDesc("Edit the active project details. (NOT YET IMPLEMENTED)");
+	SetInput("editproject");
+	SetDesc("Edit the active project details.");
 }
 
 void EditProject::OnUpdate()
 {
-	std::cout << "Command not yet implemented" << std::endl;
+	LoadSltProjXML("No projects created");
+
+	Project& activeProject = Controller::Get().GetActiveProject();
+
+	std::cout << "Editing active project - Input new details now" << std::endl;
+
+	std::string userinput;
+	std::cout << "Current Title: " << activeProject.GetTitle() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeProject.SetTitle(userinput);
+
+	std::cout << "Current Company: " << activeProject.GetCompany() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeProject.SetCompany(userinput);
+
+	std::cout << "Current Director: " << activeProject.GetDirector() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeProject.SetDirector(userinput);
+
+	std::cout << "Current DP: " << activeProject.GetDP() << std::endl;
+	UserPrompt(userinput, "Change to");
+	activeProject.SetDP(userinput);
+
+	Serializer::Get().SerializeProjectVector(Controller::Get().GetProjectVector());
 }
